1c397bd6e0bacaa99ff157e8d308aa9fefb961b0
elbywan <elbywan@hotmail.com>
2019-01-24 13:53:43 +0100

    fix typo

--- static/tests/test.spec.js
+++ static/tests/test.spec.js
@@ -11,7 +11,7 @@ it('should query a Pokemon by its id and output its name', async () => {
             createPokemonIdSlot('1')
         ]
     })
-    // In test mode, the i18n output is mocked as a JSON contining the i18n key and associated options.
+    // In test mode, the i18n output is mocked as a JSON containing the i18n key and associated options.
     // (basically the arguments passed to i18n, in serialized string form)
     const { key, options } = JSON.parse((await session.end()).text)
     expect(key).toBe('pokemon.info')
